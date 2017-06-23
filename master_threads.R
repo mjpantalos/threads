1c61f2efafba680df1d83e42d305a28c37782dad
mjpantalos <mjpantalos@users.noreply.github.com>
2017-06-23 08:17:40 -0700

    Update master_threads.R

--- master_threads.R
+++ master_threads.R
@@ -9,51 +9,12 @@ data_1 <- read.csv(file = "Specimen_RawData_7.csv", header = TRUE,
 head(data_1)
 specimen_label <- data_1[1,1] 
 
+data_1 <- read.csv(file = "Specimen_RawData_7.csv", header = TRUE, stringsAsFactors = FALSE, skip = 5)
+names(data_1)<-c("time", "extension", "load")
+
 #=====
 # Define Functions
 #=====
-
-FindPeaks<-function(points, MagPre=1, MagPost=-1){
-  x1<-NULL
-  x2<-NULL
-  x3<-NULL
-  y1<-NULL
-  y2<-NULL
-  y3<-NULL
-  s1<-NULL
-  s2<-NULL
-  peaks<-data.frame(x=c(0), y=c(0))
-  for(i in 2:(length(points$x)-1)){
-    x1<-points$x[i-1]
-    x2<-points$x[i]
-    x3<-points$x[i+1]
-    y1<-points$y[i-1]
-    y2<-points$y[i]
-    y3<-points$y[i+1]
-    s1<-(y2-y1)/(x2-x1)
-    s2<-(y3-y2)/(x3-x2)
-    if (s1>MagPre & s2<MagPost){
-      if(peaks$y[1]==0){
-        peaks$x[1]<-x2
-        peaks$y[1]<-y2
-      }else{
-        peaks<-rbind(peaks, data.frame(x=x2, y=y2))
-      }
-    }
-  }
-  return(peaks)
-}
-
-
-#=====
-# Determine peaks
-#=====
-test_data_xy<-data.frame(x=test_data$extension,y=test_data$load)
-test_peaks<-FindPeaks(points=test_data_xy, MagPre=0.2, MagPost=-0.2)
-plot(test_data_xy, pch=".")
-plot(test_data_xy, pch=".", xlim=c(9,9.25), ylim=c(0.25, 0.3))
-points(test_peaks, pch=16)
-
 
 #LowRes: reduce the number of points by taking mean
 LowRes<-function(load_data, dx=1){
@@ -79,15 +40,10 @@ LowRes<-function(load_data, dx=1){
  return(newpoints)
 }
 
-data_1 <- read.csv(file = "Specimen_RawData_7.csv", header = TRUE, stringsAsFactors = FALSE, skip = 5)
-names(data_1)<-c("time", "extension", "load")
 
-data_1_low<-LowRes(load_data=data_1, dx=0.01)
-plot(data_1$load~data_1$extension, pch=".")
-points(data_1_low, pch=16, cex=0.5)
 
-#FindPeaks_2: finds points with preceding slope greater than PreS and following slope less than PostS
-FindPeaks_2<-function(load_data, PreS=0, PostS=-1){
+#FindPeaks: finds points with preceding slope greater than PreS and following slope less than PostS
+FindPeaks<-function(load_data, PreS=0, PostS=-1){
   x1<-NULL
   x2<-NULL
   x3<-NULL
@@ -131,7 +87,8 @@ FindPeaks_2<-function(load_data, PreS=0, PostS=-1){
 }
 
 
-data_1_low_peaks<-FindPeaks_2(load_data=data_1_low, PreS=0, PostS=-1)
+data_1_low<-LowRes(load_data=data_1, dx=0.05)
+data_1_low_peaks<-FindPeaks(load_data=data_1_low, PreS=0, PostS=-0.5)
 
 plot(data_1$load~data_1$extension, pch=".")
 points(data_1_low, pch=16, cex=0.5)
