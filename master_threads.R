6e6c389af28451d3a741161e43778d3f542b91e1
mjpantalos <mjpantalos@users.noreply.github.com>
2017-06-23 05:56:15 -0700

    Update master_threads.R

--- master_threads.R
+++ master_threads.R
@@ -73,16 +73,60 @@ LowRes<-function(load_data, dx=1){
   #finding mean load during interval i
   loadmean<-mean(load_data$load[((i-1)*numpoints+1):(i*numpoints)])
   #adding mean extension and load for interval i to newpoints
-  newpoints$extention[i]<-extenmean
+  newpoints$extension[i]<-extenmean
   newpoints$load[i]<-loadmean
  }
  return(newpoints)
 }
 
+data_1 <- read.csv(file = "Specimen_RawData_7.csv", header = TRUE, stringsAsFactors = FALSE, skip = 5)
+names(data_1)<-c("time", "extension", "load")
 
+data_1_low<-LowRes(load_data=data_1, dx=0.01)
+plot(data_1$load~data_1$extension, pch=".")
+points(data_1_low, pch=16, cex=0.5)
 
-
-
-
-
+#FindPeaks_2: finds points with preceding slope greater than PreS and following slope less than PostS
+FindPeaks_2<-function(load_data, PreS=1, PostS=-1){
+  x1<-NULL
+  x2<-NULL
+  x3<-NULL
+  y1<-NULL
+  y2<-NULL
+  y3<-NULL
+  s1<-NULL
+  s2<-NULL
+  peaks<-data.frame(extension=c(0), load=c(0))
+  #loop for each point, except first and last
+  for(i in 2:(length(load_data$extension)-1)){
+    #extention of preceding point
+    x1<-load_data$extension[i-1]
+    #extension of point being considered
+    x2<-load_data$extension[i]
+    #extension of following point
+    x3<-load_data$extension[i+1]
+    #load of preceding point
+    y1<-load_data$load[i-1]
+    #load of point being considered
+    y2<-load_data$load[i]
+    #load of following point
+    y3<-load_data$load[i+1]
+    #precedding slope
+    s1<-(y2-y1)/(x2-x1)
+    #following slope
+    s2<-(y3-y2)/(x3-x2)
+    #test if matches specified slope requirements
+    if (s1>PreS & s2<PostS){
+      #for if the point is the first being considered
+      if(peaks$load[1]==0){
+        peaks$extension[1]<-x2
+        peaks$load[1]<-y2
+      #for all points being considered except the first, adds to existing points
+      }else{
+        peaks<-rbind(peaks, data.frame(extension=x2, load=y2))
+      }
+    }
+  }
+  return(peaks)
+}
 
